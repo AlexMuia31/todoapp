9b1466a9913727a2713f33fe495a8b198b8a2bf0
AlexMuia31 <alexmuia31@gmail.com>
2021-04-27 15:36:10 +0300

    changes for heroku

--- todo/settings.py
+++ todo/settings.py
@@ -126,10 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'portfolio/static')
+    os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
