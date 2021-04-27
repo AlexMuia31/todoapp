eb26e7176de8b364fdde4a9b488bfc562ea1bd1b
AlexMuia31 <alexmuia31@gmail.com>
2021-04-27 15:25:50 +0300

    changes for heroku

--- todo/urls.py
+++ todo/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from todoapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('del/<int:item_id>', views.remove,  name="del"),
     path('', views.index, name="todo"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
